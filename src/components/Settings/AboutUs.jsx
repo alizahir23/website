23eede287cc4a43cf161c4431738e15cb6ae5974
alizahir23 <alizahiruddin@gmail.com>
2020-05-19 13:56:26 +0530

    added skill functionality in about you section

--- src/components/Settings/AboutUs.jsx
+++ src/components/Settings/AboutUs.jsx
@@ -1,29 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../css/settings.module.css';
 
-const Aboutus = () => (
-  <div>
-    <div className={styles['basic-head']}>
-      <p>What do you do?</p>
+const Aboutus = () => {
+  const [tag, setTag] = useState('');
+  const [tags, setTags] = useState([]);
+
+  const onChange = (e) => {
+    setTag(e.target.value);
+  };
+
+  const removeTag = (indexToRemove) => {
+    setTags([...tags.filter((element, index) => index !== indexToRemove)]);
+  };
+
+  return (
+    <div>
+      <div className={styles['basic-head']}>
+        <p>What do you do?</p>
+      </div>
+      <div className={styles.qns}>
+        <p>Title</p>
+        <input
+          className={styles.input}
+          placeholder="Developer, Student, Programmer"
+        />
+        <p>About</p>
+        <input
+          className={styles['input-bio']}
+          placeholder="A short bio of less than 120 characters"
+        />
+        <p>Skills</p>
+        <form
+          className={styles.skills}
+          onSubmit={(e) => {
+            setTags([...tags, tag]);
+            setTag('');
+            e.target.reset();
+            e.preventDefault();
+          }}
+        >
+          <input
+            className={styles.input}
+            id="myInput"
+            placeholder="Enter your skills"
+            onKeyUp={(e) => onChange(e)}
+          />
+          <input type="submit" className={styles.addButton} value="+" />
+        </form>
+        <ul className={styles.skillList}>
+          {tags.map((Tag, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={index} className={styles.skill}>
+              <li>{Tag}</li>
+              <div
+                style={{
+                  cursor: 'pointer',
+                  display: 'inline-block',
+                  fontWeight: '600',
+                }}
+                role="button"
+                tabIndex={0}
+                onClick={() => removeTag(index)}
+                onKeyDown={() => removeTag(index)}
+              >
+                x
+              </div>
+            </div>
+          ))}
+        </ul>
+      </div>
     </div>
-    <div className={styles.qns}>
-      <p>Title</p>
-      <input
-        className={styles.input}
-        placeholder="Developer, Student, Programmer"
-      />
-      <p>About</p>
-      <input
-        className={styles['input-bio']}
-        placeholder="A short bio of less than 120 characters"
-      />
-      <p>Skills</p>
-      <input
-        className={styles.input}
-        id="myInput"
-        placeholder="Enter your skills"
-      />
-    </div>
-  </div>
-);
+  );
+};
 export default Aboutus;
