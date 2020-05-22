f864ef2e05a4777c7cffa3186a880430275d7c7e
Ali Zahir <alizahiruddin@gmail.com>
2020-05-22 16:36:48 +0530

    Removed card component and improved profile responsiveness (#55)
    * removed card component and minor changes
    
    * review 1
    
    * converted grid to flex

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import Card from '../src/components/FeedCard';
-// import ProjectProfile from '../src/components/profile/projectProfile';
-
 import Header from '../src/components/Header';
 import BoxProfile from '../src/components/profile/profilebox';
 
@@ -11,7 +8,6 @@ function Profile() {
     <div>
       <Header />
       <BoxProfile />
-      <Card />
     </div>
   );
 }
