b9c4314576ad046f36056f5db78d162957dfefb2
alizahir23 <alizahiruddin@gmail.com>
2020-06-09 20:25:38 +0530

    added firestore to profile page and fixed memory leak in totop

--- src/components/FirebaseAuth.js
+++ src/components/FirebaseAuth.js
@@ -7,11 +7,17 @@ export async function GoogleSignIn() {
   /*
       Define Required  Scopes here
   */
+  return firebase.auth().signInWithPopup(provider).then((result) => {
+    const db = firebase.firestore();
+    db.collection('users').doc(result.user.uid).set({
+      name: result.user.displayName,
+      email: result.user.email,
+      uid: result.user.uid,
+    });
 
-  return firebase.auth().signInWithPopup(provider).then((result)=>{
     const resultData = { email: result.user.email, name: result.user.displayName, uid: result.user.uid };
     const newSecureToken = jwt.sign(resultData, process.env.NEXT_PUBLIC_SECURE_TOKEN_ACCESS_KEY);
-    localStorage.setItem('osc-app-token',newSecureToken);
+    localStorage.setItem('osc-app-token', newSecureToken);
     return result;
   }).catch((error) => {
     return error;
@@ -21,7 +27,7 @@ export async function GoogleSignIn() {
 export async function GithubSignIn() {
 
   const provider = new firebase.auth.GithubAuthProvider();
-  return firebase.auth().signInWithPopup(provider).then((result) =>{
+  return firebase.auth().signInWithPopup(provider).then((result) => {
     const resultData = { email: result.user.email, name: result.user.displayName, uid: result.user.uid };
     const newSecureToken = jwt.sign(resultData, process.env.NEXT_PUBLIC_SECURE_TOKEN_ACCESS_KEY);
     localStorage.setItem('osc-app-token', newSecureToken);
@@ -34,9 +40,9 @@ export async function GithubSignIn() {
 
 export async function logout() {
   localStorage.removeItem('osc-app-token');
-  return firebase.auth().signOut().then(()=>{
+  return firebase.auth().signOut().then(() => {
     return "Success";
-  }).catch(()=>{
+  }).catch(() => {
     return "Error"
   });
 }
@@ -47,11 +53,11 @@ export async function getCurrentUser() {
 
 export async function verifySecuredToken(token) {
 
-  return jwt.verify(token, process.env.NEXT_PUBLIC_SECURE_TOKEN_ACCESS_KEY, (err,userData) => {
+  return jwt.verify(token, process.env.NEXT_PUBLIC_SECURE_TOKEN_ACCESS_KEY, (err, userData) => {
     if (err)
       return null;
     if (userData.name === (null || undefined) || userData.email === (null || undefined) || userData.uid === (null || undefined))
-    return null;  
+      return null;
     return userData;
   });
 }
--- src/components/profile/profilebox.jsx
+++ src/components/profile/profilebox.jsx
@@ -1,108 +1,137 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
+import firebase from '../../firebase';
 import styles from '../../scss/profile.module.scss';
+import UserContext from '../UserContext';
 
 export default function BoxProfile() {
+  const db = firebase.firestore();
+  const { User } = useContext(UserContext);
+  const [Loading, setLoading] = useState(true);
+  const [UserData, setUserData] = useState({});
+  // const [MissingData, setMissingData] = useState(false);
+
+  useEffect(() => {
+    if (User)
+      db.collection('users')
+        .doc(User.uid)
+        .get()
+        .then((data) => {
+          setUserData(data.data());
+          setLoading(false);
+        });
+  }, [User]);
+
+  if (Loading)
+    return (
+      <div className={styles.loading}>
+        <img src="/SVG/loading.svg" alt="loading..." />
+      </div>
+    );
   return (
-    <div>
-      <div className={styles['usernamebox-profile']}>
-        <div className={styles['user-flex']}>
-          <div className={styles['top-left-col']}>
-            <img
-              className={styles.imgabsolute}
-              src="SVG/Rectangle 60.svg"
-              alt="Profile pic"
-            />
-
-            <div className={styles.boxcontent}>
-              <h2 className={styles['usersname-user']}>Arya Soni</h2>
-              <hr
-                className={styles.hr}
-                style={{ width: '20%', color: '#707070' }}
+      <div>
+        {/* {MissingData ? (
+          <div className={styles.alert}>Update your profile...</div>
+        ) : null} */}
+        <div className={styles['usernamebox-profile']}>
+          <div className={styles['user-flex']}>
+            <div className={styles['top-left-col']}>
+              <img
+                className={styles.imgabsolute}
+                src="SVG/Rectangle 60.svg"
+                alt="Profile pic"
               />
-              <p style={{ fontWeight: '200' }}>@aryasoni98</p>
 
-              <p className={styles['username-skill']}>Developer</p>
+              <div className={styles.boxcontent}>
+                <h2 className={styles['usersname-user']}>{UserData.name}</h2>
+                <hr
+                  className={styles.hr}
+                  style={{ width: '20%', color: '#707070' }}
+                />
+                <p style={{ fontWeight: '200' }}>@{UserData.userName}</p>
+
+                <p className={styles['username-skill']}>{UserData.title}</p>
+                <div className={styles.langsec}>
+                  {UserData.skills.map((skill) => {
+                    return (
+                      <div key={skill} className={styles.skill}>
+                        <p>{skill}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.links}>
+              <div>
+                <a href={UserData.github} target="blank">
+                  <img src="SVG/social-media.svg" alt="github" />
+                </a>
+              </div>
+              <div className={styles['top-right-in']}>
+                <a href={UserData.linkedin}>
+                  <img src="SVG/Linkedin.svg" alt="linkedin" />
+                </a>
+              </div>
+              <div className={styles['top-right-in']}>
+                <a href={UserData.twitter}>
+                  <img src="SVG/brands-and-logotypes.svg" alt="twitter" />
+                </a>
+              </div>
             </div>
           </div>
 
-          <div className={styles.links}>
-            <div>
-              <img src="SVG/social-media.svg" alt="github" />
-            </div>
-            <div className={styles['top-right-in']}>
-              <img src="SVG/Linkedin.svg" alt="linkedin" />
-            </div>
-            <div className={styles['top-right-in']}>
-              <img src="SVG/brands-and-logotypes.svg" alt="linkedin" />
-            </div>
+          <div className={styles.usercontent}>
+            {UserData.about ? <p>{UserData.about}</p> : <p>No about...</p>}
           </div>
-        </div>
 
-        <div className={styles.usercontent}>
-          <p>
-            Something about the user. User will give it later.Dont be scared of
-            Something about the user. User will give it later.Dont be scared of
-            Something about the user. User will give it later.Dont be scared of
-            Something about the user. User will give it later.Dont be scared of
-          </p>
-        </div>
-
-        <div className={styles.langsec}>
-          <div>
-            <h3 className={styles.userskills}>Skills</h3>
-          </div>
-          <div className={styles.langButtonJS}>
-            <p>Javascript</p>
-          </div>
-          <div className={styles.langButtonCSS}>
-            <p>CSS</p>
-          </div>
-        </div>
-
-        <div className={styles['langsec-centre']}>
-          <div className={styles.smallbox}>
-            <div className={styles.langsec}>
-              <div className={styles.smallboxwriting}>
-                <p>Follow</p>
-              </div>
-              <div className={styles.count}>
-                <p>1</p>
+          <div className={styles['langsec-centre']}>
+            <div className={styles.smallbox}>
+              <div className={styles.langsec}>
+                <div className={styles.smallboxwriting}>
+                  <p>Follow</p>
+                </div>
+                <div className={styles.count}>
+                  <p>
+                    {UserData.followingOrganisations.length +
+                      UserData.followingLanguages.length}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={styles.smallbox}>
-            <div className={styles.langsec}>
-              <div className={styles.smallboxwriting}>
-                <p>Likes</p>
-              </div>
-              <div className={styles.count}>
-                <p>5</p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.smallbox}>
-            <div className={styles.langsec}>
-              <div className={styles.smallboxwriting}>
-                <p>Save</p>
-              </div>
-              <div className={styles.count}>
-                <p>5</p>
+            <div className={styles.smallbox}>
+              <div className={styles.langsec}>
+                <div className={styles.smallboxwriting}>
+                  <p>Likes</p>
+                </div>
+                <div className={styles.count}>
+                  <p>0</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className={styles.smallbox}>
-            <div className={styles.langsec}>
-              <div className={styles.smallboxwriting}>
-                <p>OSP</p>
+            <div className={styles.smallbox}>
+              <div className={styles.langsec}>
+                <div className={styles.smallboxwriting}>
+                  <p>Save</p>
+                </div>
+                <div className={styles.count}>
+                  <p>0</p>
+                </div>
               </div>
-              <div className={styles.count}>
-                <p>1</p>
+            </div>
+            <div className={styles.smallbox}>
+              <div className={styles.langsec}>
+                <div className={styles.smallboxwriting}>
+                  <p>OSP</p>
+                </div>
+                <div className={styles.count}>
+                  <p>1</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
