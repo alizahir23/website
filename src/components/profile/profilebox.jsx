058182b4d906f692cbb618aee78ee13a923d0799
alizahir23 <alizahiruddin@gmail.com>
2020-06-10 20:17:11 +0530

    empty fields are now being handled

--- src/components/profile/profilebox.jsx
+++ src/components/profile/profilebox.jsx
@@ -29,109 +29,124 @@ export default function BoxProfile() {
       </div>
     );
   return (
-      <div>
-        {/* {MissingData ? (
+    <div>
+      {/* {MissingData ? (
           <div className={styles.alert}>Update your profile...</div>
         ) : null} */}
-        <div className={styles['usernamebox-profile']}>
-          <div className={styles['user-flex']}>
-            <div className={styles['top-left-col']}>
-              <img
-                className={styles.imgabsolute}
-                src="SVG/Rectangle 60.svg"
-                alt="Profile pic"
+      <div className={styles['usernamebox-profile']}>
+        <div className={styles['user-flex']}>
+          <div className={styles['top-left-col']}>
+            <img
+              className={styles.imgabsolute}
+              src="SVG/Rectangle 60.svg"
+              alt="Profile pic"
+            />
+
+            <div className={styles.boxcontent}>
+              <h2 className={styles['usersname-user']}>
+                {UserData.name && UserData.name}
+              </h2>
+              <hr
+                className={styles.hr}
+                style={{ width: '20%', color: '#707070' }}
               />
+              <p style={{ fontWeight: '200' }}>
+                {UserData.userName && `@${UserData.userName}`}
+              </p>
 
-              <div className={styles.boxcontent}>
-                <h2 className={styles['usersname-user']}>{UserData.name}</h2>
-                <hr
-                  className={styles.hr}
-                  style={{ width: '20%', color: '#707070' }}
-                />
-                <p style={{ fontWeight: '200' }}>@{UserData.userName}</p>
-
-                <p className={styles['username-skill']}>{UserData.title}</p>
-                <div className={styles.langsec}>
-                  {UserData.skills.map((skill) => {
-                    return (
-                      <div key={skill} className={styles.skill}>
-                        <p>{skill}</p>
-                      </div>
-                    );
-                  })}
-                </div>
+              <p className={styles['username-skill']}>{UserData.title}</p>
+              <div className={styles.langsec}>
+                {UserData.skills
+                  ? UserData.skills.map((skill) => {
+                      return (
+                        <div key={skill} className={styles.skill}>
+                          <p>{skill}</p>
+                        </div>
+                      );
+                    })
+                  : 'No skills...'}
               </div>
             </div>
+          </div>
 
-            <div className={styles.links}>
+          <div className={styles.links}>
+            {UserData.github && (
               <div>
                 <a href={UserData.github} target="blank">
                   <img src="SVG/social-media.svg" alt="github" />
                 </a>
               </div>
+            )}
+            {UserData.linkedin && (
               <div className={styles['top-right-in']}>
                 <a href={UserData.linkedin}>
                   <img src="SVG/Linkedin.svg" alt="linkedin" />
                 </a>
               </div>
+            )}
+            {UserData.twitter && (
               <div className={styles['top-right-in']}>
                 <a href={UserData.twitter}>
-                  <img src="SVG/brands-and-logotypes.svg" alt="twitter" />
+                  <img src="SVG/twitter.svg" alt="twitter" />
                 </a>
               </div>
+            )}
+          </div>
+        </div>
+
+        <div className={styles.usercontent}>
+          {UserData.about ? <p>{UserData.about}</p> : <p>No about...</p>}
+        </div>
+
+        <div className={styles['langsec-centre']}>
+          <div className={styles.smallbox}>
+            <div className={styles.langsec}>
+              <div className={styles.smallboxwriting}>
+                <p>Follow</p>
+              </div>
+              <div className={styles.count}>
+                <p>
+                  {UserData.followingOrganisations !== undefined ||
+                  UserData.followingLanguages !== undefined
+                    ? UserData.followingOrganisations.length +
+                      UserData.followingLanguages.length
+                    : 0}
+                </p>
+              </div>
             </div>
           </div>
-
-          <div className={styles.usercontent}>
-            {UserData.about ? <p>{UserData.about}</p> : <p>No about...</p>}
+          <div className={styles.smallbox}>
+            <div className={styles.langsec}>
+              <div className={styles.smallboxwriting}>
+                <p>Likes</p>
+              </div>
+              <div className={styles.count}>
+                <p>0</p>
+              </div>
+            </div>
           </div>
-
-          <div className={styles['langsec-centre']}>
-            <div className={styles.smallbox}>
-              <div className={styles.langsec}>
-                <div className={styles.smallboxwriting}>
-                  <p>Follow</p>
-                </div>
-                <div className={styles.count}>
-                  <p>
-                    {UserData.followingOrganisations.length +
-                      UserData.followingLanguages.length}
-                  </p>
-                </div>
+          <div className={styles.smallbox}>
+            <div className={styles.langsec}>
+              <div className={styles.smallboxwriting}>
+                <p>Save</p>
+              </div>
+              <div className={styles.count}>
+                <p>0</p>
               </div>
             </div>
-            <div className={styles.smallbox}>
-              <div className={styles.langsec}>
-                <div className={styles.smallboxwriting}>
-                  <p>Likes</p>
-                </div>
-                <div className={styles.count}>
-                  <p>0</p>
-                </div>
+          </div>
+          <div className={styles.smallbox}>
+            <div className={styles.langsec}>
+              <div className={styles.smallboxwriting}>
+                <p>OSP</p>
               </div>
-            </div>
-            <div className={styles.smallbox}>
-              <div className={styles.langsec}>
-                <div className={styles.smallboxwriting}>
-                  <p>Save</p>
-                </div>
-                <div className={styles.count}>
-                  <p>0</p>
-                </div>
-              </div>
-            </div>
-            <div className={styles.smallbox}>
-              <div className={styles.langsec}>
-                <div className={styles.smallboxwriting}>
-                  <p>OSP</p>
-                </div>
-                <div className={styles.count}>
-                  <p>1</p>
-                </div>
+              <div className={styles.count}>
+                <p>1</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
