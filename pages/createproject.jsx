25420aae01a975cb6dbfcea30dfb2af684a4e66e
Vipul Sharma <Vipulsnp@gmail.com>
2020-06-23 21:15:47 +0530

    added components in createProject (#131)

--- pages/createproject.jsx
+++ pages/createproject.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import Header from '../src/components/Header';
+import FormOsp from '../src/components/Osp/ospForm';
+import HeadOsp from '../src/components/Osp/ospHead';
 
 const createproject = () => (
   <div>
     <Header />
+    <HeadOsp />
+    <FormOsp />
   </div>
 );
 export default createproject;
