fcaebc9a4a2cac5ed25c9e3f57270bb1bf4399bc
Vipul Sharma <Vipulsnp@gmail.com>
2020-06-25 03:44:12 +0530

    Fixed SkillList display Error and Added Form Validation (#136)
    * Fixed SkillList display Error and Added Form Validation
    
    * Minor Change
    
    * Changes Made
    
    Co-authored-by: Irshad Ansari <40335357+irshadjsr21@users.noreply.github.com>

--- src/components/Osp/ospForm.jsx
+++ src/components/Osp/ospForm.jsx
@@ -1,13 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
+import * as FormValidation from '../../FormValidation';
 import styles from '../../scss/osp.module.scss';
 
 const FormOsp = () => {
   const [tag, setTag] = useState('');
   const [tags, setTags] = useState([]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [github, setGithub] = useState('');
+  const [hostedUrl, setHostedUrl] = useState('');
+  const [titleError, setTitleError] = useState(null);
+  const [descriptionError, setDescriptionError] = useState(null);
+  const [githubError, setGithubError] = useState(null);
+  const [hostedUrlError, setHostedUrlError] = useState(null);
+  const [skillError, setSkillError] = useState(null);
+  const [addSkillButtonDisabled, setAddSkillButtonDisabled] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);      // Submit Button Disabled
 
+  useEffect(() => {
+    if ((titleError === null) && (descriptionError === null) && (githubError === null) && (hostedUrlError === null) &&
+        (title.length>0) && (description.length>0) && (github.length>0)) {
+      setIsDisabled(false);
+    }
+    else {
+      setIsDisabled(true);
+    }
+  }, [titleError, descriptionError, githubError, hostedUrlError]);
+
+  useEffect(() => {
+    if (skillError === null)
+      setAddSkillButtonDisabled(false);
+    else
+      setAddSkillButtonDisabled(true);
+  }, [skillError]);
 
   const onChange = (e) => {
+    const found = tags.find(el => el.toUpperCase() === e.target.value.toUpperCase());
+    if (found !== undefined) {
+      setSkillError("This skill is already present.");
+    }
+    else
+      setSkillError(null);
     setTag(e.target.value);
   };
 
@@ -15,27 +49,46 @@ const FormOsp = () => {
   const removeTag = (indexToRemove) => {
     setTags([...tags.filter((element, index) => index !== indexToRemove)]);
   };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    // eslint-disable-next-line
+    console.log(title, description, github, tags, hostedUrl);
+    
+  }
+
   return (
     <div>
       <div className={styles.bigBox}>
         <div className={styles.flexingin}>
           <div className={styles.qns}>
             <div style={{ color: 'blue' }}>
-              <p>Title</p>
+              <p>Title<sup> * </sup></p>
             </div>
             <input
-              className={styles.input}
+              className={`${styles.input} ${titleError !== null ? styles.invalid : ''} `}
               placeholder="Title of the project"
               style={{ borderBottomColor: 'blue' }}
+              onChange={(e) => {
+                setTitle(e.currentTarget.value);
+                setTitleError(FormValidation.checkLengthLimit(e.currentTarget.value.length, 50, 1));
+              }}
             />
+            <p id='titleError' className='input-field-error'>{titleError}</p>
             <div style={{ color: '#00CACA' }}>
-              <p>Description</p>
+              <p>Description<sup> * </sup></p>
             </div>
-            <input
-              className={styles.input}
+            <textarea
+              className={`${styles.input} ${descriptionError !== null ? styles.invalid : ''} `}
               placeholder="About your project"
               style={{ borderBottomColor: '#00CACA' }}
+              rows="4"
+              onChange={(e) => {
+                setDescription(e.currentTarget.value);
+                setDescriptionError(FormValidation.checkLengthLimit(e.currentTarget.value.length, 250, 25));
+              }}
             />
+            <p id='descriptionError' className='input-field-error'>{descriptionError}</p>
             <div style={{ color: '#0BA293' }}>
               <p>Skills</p>
             </div>
@@ -49,14 +102,16 @@ const FormOsp = () => {
               }}>
               <input
                 required
-                className={styles.input}
+                className={`${styles.input} ${skillError !== null ? styles.invalid : ''} `}
                 id="myInput"
                 placeholder="Enter the skills"
                 style={{ borderBottomColor: '#0BA293' }}
+                autoComplete="off"
                 onKeyUp={(e) => onChange(e)}
               />
-              <input type="submit" className={styles.addButton} value="+" />
+              <input type="submit" className={styles.addButton} disabled={addSkillButtonDisabled} value="+" />
             </form>
+            <p id='skillError' className='input-field-error'>{skillError}</p>
             <ul className={styles.skillList}>
               {tags.map((Tag, index) => (
                 // eslint-disable-next-line react/no-array-index-key
@@ -77,7 +132,7 @@ const FormOsp = () => {
             <div style={{ color: 'black' }}>
               <div className={styles.flex}>
                 <div>
-                  <p>Github</p>
+                  <p>Github<sup> * </sup></p>
                 </div>
                 <div>
                   <img src="SVG/git.svg" alt="git" />
@@ -85,10 +140,19 @@ const FormOsp = () => {
               </div>
             </div>
             <input
-              className={styles.input}
+              className={`${styles.input} ${githubError !== null ? styles.invalid : ''} `}
               placeholder="www.github.com/username"
               style={{ borderBottomColor: 'black' }}
+              onChange={(e) => {
+                if (e.currentTarget.value.length === 0) {
+                  setGithubError('This field is required.');
+                  return;
+                }
+                setGithub(e.currentTarget.value);
+                setGithubError(FormValidation.checkUrl(e.currentTarget.value));
+              }}
             />
+            <p id='githubError' className='input-field-error'>{githubError}</p>
             <div style={{ color: '#9F0BDD' }}>
               <div className={styles.flex}>
                 <div>
@@ -100,10 +164,15 @@ const FormOsp = () => {
               </div>
             </div>
             <input
-              className={styles.input}
+              className={`${styles.input} ${hostedUrlError !== null ? styles.invalid : ''} `}
               placeholder="Hosting URL of the project"
               style={{ borderBottomColor: '#9F0BDD' }}
+              onChange={(e) => {
+                setHostedUrl(e.currentTarget.value);
+                setHostedUrlError(FormValidation.checkUrl(e.currentTarget.value));
+              }}
             />
+            <p id='hostedUrlError' className='input-field-error'>{hostedUrlError}</p>
             <p>Upload Your Project Here</p>
 
             <label htmlFor="file">
@@ -122,7 +191,7 @@ const FormOsp = () => {
           </div>
         </div>
         <div>
-          <button type="button" className={styles['enter-button']}>
+          <button type="button" onClick={handleFormSubmit} disabled={isDisabled} className={styles['enter-button']}>
             Submit
           </button>
         </div>
